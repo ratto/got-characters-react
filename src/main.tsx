242d73590bd8bcd6bf92943c1fa9d0ff6cda6d73
ratto <rattopedro@gmail.com>
2024-05-14 14:46:01 -0300

    Styling a bit and getting rid of that whitish interface

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
