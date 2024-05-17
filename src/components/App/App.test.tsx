48ea89507ad57a931efc0ab84da951736a2a2d43
ratto <rattopedro@gmail.com>
2024-05-17 19:07:14 -0300

    App unit tested

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,5 +1,59 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+import Character from '../../types/Character.ts';
+import App from './index.tsx';
+import { loadCharacters } from '../../utils/load-characters.ts';
+
+vi.mock('../../utils/load-characters.ts', () => ({
+  loadCharacters: vi.fn(),
+}));
+
+vi.mock('../CharactersDeck', () => ({
+  CharactersDeck: ({ characters }: { characters: Character[] }) => (
+    <div data-testid="characters-deck">
+      {characters.map(character => (
+        <div key={character.id}>
+          {character.firstName} {character.lastName}
+        </div>
+      ))}
+    </div>
+  ),
+}));
+
+const mockedCharacters = [
+  { id: 1, firstName: 'Jon', lastName: 'Snow' },
+  { id: 2, firstName: 'Daenerys', lastName: 'Targaryen' },
+];
+
 describe('App test suite', () => {
-  it('is just a dummy test', () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    (loadCharacters as jest.Mock).mockResolvedValue(mockedCharacters);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should render App component', () => {
+    render(<App />);
+
+    const component = screen.getByText('Game of Thrones Character List');
+
+    expect(component).toBeInTheDocument();
+  });
+
+  it('should render all the characters', async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      expect(loadCharacters).toHaveBeenCalledTimes(1);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('characters-deck')).toBeInTheDocument();
+      expect(screen.getByText('Jon Snow')).toBeInTheDocument();
+      expect(screen.getByText('Daenerys Targaryen')).toBeInTheDocument();
+    });
   });
 });
